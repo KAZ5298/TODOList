d1f6707afdebdc7605afdb5c9695ce2fc01a813f
KAZUHIKO <kazuhiko.d@gmail.com>
2023-07-25 13:38:33 +0900

    テスト用SQL修正

--- sql/insert_table_users.sql
+++ sql/insert_table_users.sql
@@ -2,18 +2,28 @@ insert into `users` (
     `user`,
     `pass`,
     `family_name`,
-    `first_name`
+    `first_name`,
+    `is_admin`
 )
 values
 (
     'test1',
     '$2y$10$eSePpwz2hteTQZNXO1BvFeI.VCSGF/YqGdpZda/sHQDQWzAJoehYi', -- パスワード test1
     'テスト',
-    '花子'
+    '花子',
+    0
 ),
 (
     'test2',
     '$2y$10$btIzYtozzeEJ2J53ZU/Qz.YBK61RilXtGcVJkrZfz1r/fS8R72F.i', -- パスワード test2
     'テスト',
-    '太郎'
+    '太郎',
+    0
+);
+(
+    'test3',
+    '$2y$10$pQ99VXOdPt3Dg6TEkUtzPeFeROzGB9RJbkkk2AhcovvEuDQXmuCo.', -- パスワード test3
+    'テスト',
+    '管理者',
+    1
 );
