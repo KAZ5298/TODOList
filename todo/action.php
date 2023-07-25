e8bfe109e78b7626acb5156721170b84abbd88be
KAZ5298 <kazuhiko.d@gmail.com>
2023-07-25 18:17:16 +0900

    バリデーションチェック

--- todo/action.php
+++ todo/action.php
@@ -32,51 +32,23 @@ try {
 
     // バリデーションチェック
 
-    // if (empty($post['user_id'])) {
-    //     $_SESSION['err_msg'] = '担当者を選択してください。';
-    //     header('Location:' . $_SERVER['HTTP_REFERER']);
-    //     exit;
-    // }
-
     if (!Validation::userNullCheck($post['user_id'])) {
         $_SESSION['err_msg'] = '担当者を選択してください。';
-        header("Location: ${url}");
+        header("Location: ${url}", true, 307);
         exit;
     }
 
-    // if (!Validation::itemNullCheck($post['item_name'])) {
-    //     $_SESSION['err_msg'] = '項目名が空白です。';
-    //     if ($post['action'] == 'entry') {
-    //         header('Location: ./entry.php');
-    //         exit;
-    //     } elseif ($post['action'] == 'edit') {
-    //         header('Location: ./edit.php');
-    //         exit;
-    //     }
-    // }
+    if (!Validation::itemNullCheck($post['item_name'])) {
+        $_SESSION['err_msg'] = '項目名が空白です。';
+        header("Location: ${url}", true, 307);
+        exit;
+    }
 
     if (!Validation::strLenChk($post['item_name'])) {
         $_SESSION['err_msg'] = '項目名は１００文字以下で登録してください。';
-        header("Location: ${url}");
+        header("Location: ${url}", true, 307);
         exit;
     }
-
-    if (empty($post['item_name'])) {
-        $_SESSION['err_msg'] = '項目名が空白です。';
-        header("Location: ${url}");
-        exit;
-    }
-
-    // if (strlen($post['item_name']) > 100) {
-    //     $_SESSION['err_msg'] = '項目名は１００文字以下で登録してください。';
-    //     if ($post['action'] == 'entry') {
-    //         header('Location: ./entry.php');
-    //         exit;
-    //     } elseif ($post['action'] == 'edit') {
-    //         header('Location: ./edit.php');
-    //         exit;
-    //     }
-    // }
 
     // 登録・修正の共通処理
     if (isset($post['finished'])) {
@@ -88,28 +60,28 @@ try {
 
     // 登録・修正・完了・削除
     switch ($post['action']) {
-            // 登録
+        // 登録
         case 'entry':
             $db = new TodoItems();
             $db->insertTodoItem($post['user_id'], $post['item_name'], $dt, $post['expire_date'], $finished_date);
 
             header('Location: ./index.php');
             exit;
-            // 修正
+        // 修正
         case 'edit':
             $db = new TodoItems();
             $db->editTodoItem($post['item_id'], $post['user_id'], $post['item_name'], $post['expire_date'], $finished_date);
 
             header('Location: ./index.php');
             exit;
-            // 削除
+        // 削除
         case 'delete':
             $db = new TodoItems();
             $db->deleteTodoItem($post['item_id']);
 
             header('Location: ./index.php');
             exit;
-            // 完了
+        // 完了
         case 'complete':
             $db = new TodoItems();
             $db->todoItemIsComplete($dt, $post['item_id']);
